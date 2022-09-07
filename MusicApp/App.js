1f426d4924a3c5302b5e989641076680a4d2f028
SedanurAkçil <66331295+sedanurakcil@users.noreply.github.com>
2022-09-08 00:30:34 +0300

    musicapp added

--- MusicApp/App.js
+++ MusicApp/App.js
@@ -1,4 +1,10 @@
-
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
 import React from 'react';
 import type {Node} from 'react';
@@ -12,8 +18,16 @@ import {
   View,
 } from 'react-native';
 
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
